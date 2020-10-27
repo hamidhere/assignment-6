49cb5662c7927e266a3f2789204002ce8468287a
hamidhere <hamiddot47@icloud.com>
2020-10-28 01:39:27 +0500

    Assignment 6

---
+++ src/algorithms/java/Main.java
@@ -0,0 +1,15 @@
+package algorithms.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Sum of the numbers is: ");
+        int num1=15;
+        int num2=17;
+        int num3=17;
+        int x=num1+num2+num3;
+        System.out.println(x);
+
+                    }
+}
